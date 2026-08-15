d13a2aaa2b650ef9c9a968acb95c3989e8fd39da
VulicAzluk <18114055256@163.com>
2026-08-15 13:38:27 +0800

    Changed: clanged function APIs

--- Ulix/UApplication.hpp
+++ Ulix/UApplication.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
-#include "vulkan/vulkan_core.h"
 #include <URenderInfo.hpp>
 #include <UTimer.hpp>
 #include <URenderScene.hpp>
 #include <cassert>
-#include <print>
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
